7765b0269aa9e8ec0d5c962cd2f52a6a438c3643
grastagg <grastagg33@gmail.com>
2025-01-15 18:05:38 -0700

    i think dubins BEZ is working?

--- testDubins.py
+++ testDubins.py
@@ -92,6 +92,30 @@ def temp_BEZ(
     return distanceToGoal < (captureRadius + pursuerRange)
 
 
+def clockwise_angle(v1, v2):
+    # Calculate determinant and dot product
+    det = v1[0] * v2[1] - v1[1] * v2[0]
+    dot = v1[0] * v2[0] + v1[1] * v2[1]
+
+    # Compute angle and normalize to [0, 2*pi]
+    angle = np.arctan2(det, dot)
+    angle_ccw = (angle + 2 * np.pi) % (2 * np.pi)
+
+    return angle_ccw
+
+
+def counterclockwise_angle(v1, v2):
+    # Calculate determinant and dot product
+    det = v1[0] * v2[1] - v1[1] * v2[0]
+    dot = v1[0] * v2[0] + v1[1] * v2[1]
+
+    # Compute clockwise angle
+    angle = np.arctan2(-det, dot)
+    angle_cw = (angle + 2 * np.pi) % (2 * np.pi)
+
+    return angle_cw
+
+
 def find_dubins_path_length(startPosition, startHeading, goalPosition, radius):
     leftCenter = np.array(
         [
@@ -106,24 +130,45 @@ def find_dubins_path_length(startPosition, startHeading, goalPosition, radius):
         ]
     )
 
-    if np.linalg.norm(goalPosition - leftCenter) > np.linalg.norm(
+    clockwise = False
+    if np.linalg.norm(goalPosition - leftCenter) < np.linalg.norm(
         goalPosition - rightCenter
     ):
-        tangentPoint = find_counter_clockwise_tangent_point(
-            goalPosition, leftCenter, radius
-        )
         centerPoint = leftCenter
+        clockwise = False
+        if np.linalg.norm(goalPosition - leftCenter) < radius:
+            clockwise = True
+            centerPoint = rightCenter
+
     else:
-        tangentPoint = find_clockwise_tangent_point(goalPosition, rightCenter, radius)
         centerPoint = rightCenter
+        clockwise = True
+        if np.linalg.norm(goalPosition - rightCenter) < radius:
+            clockwise = False
+            centerPoint = leftCenter
+
+    if clockwise:
+        tangentPoint = find_clockwise_tangent_point(goalPosition, centerPoint, radius)
+    else:
+        tangentPoint = find_counter_clockwise_tangent_point(
+            goalPosition, centerPoint, radius
+        )
 
     v4 = startPosition - centerPoint
     v3 = tangentPoint - centerPoint
-    cosTheta = np.dot(v3, v4) / (np.linalg.norm(v3) * np.linalg.norm(v4))
-    theta = np.arccos(cosTheta)
+    if clockwise:
+        theta = clockwise_angle(v3, v4)
+    else:
+        theta = counterclockwise_angle(v3, v4)
+    # cosTheta = np.dot(v3, v4) / (np.linalg.norm(v3) * np.linalg.norm(v4))
+    # theta = np.arccos(cosTheta)
+    # if np.cross(v3, v4) < 0:
+    #     print("TEST")
+    #     theta = 2 * np.pi - theta
+    # print("Theta: ", theta)
 
     straitLineLength = np.linalg.norm(goalPosition - tangentPoint)
-    arcLength = radius * theta
+    arcLength = radius * np.abs(theta)
 
     totalLength = arcLength + straitLineLength
 
@@ -135,6 +180,8 @@ def find_dubins_path_length(startPosition, startHeading, goalPosition, radius):
         plt.scatter(*goalPosition, c="r")
         plt.scatter(*leftCenter, c="b")
         plt.scatter(*rightCenter, c="b")
+        plt.plot(*v3, "b")
+        plt.plot(*v4, "b")
         theta = np.linspace(0, 2 * np.pi, 100)
         xl = leftCenter[0] + radius * np.cos(theta)
         yl = leftCenter[1] + radius * np.sin(theta)
@@ -150,7 +197,6 @@ def find_dubins_path_length(startPosition, startHeading, goalPosition, radius):
         )
         ax = plt.gca()
         ax.set_aspect("equal", "box")
-        plt.show()
     return totalLength
 
 
@@ -177,6 +223,22 @@ def in_dubins_engagement_zone(
     return dubinsPathLength < (captureRadius + pursuerRange)
 
 
+def collision_region(
+    startPosition,
+    startHeading,
+    turnRadius,
+    captureRadius,
+    pursuerRange,
+    pursuerSpeed,
+    evaderSpeed,
+    evaderPosition,
+):
+    dubinsPathLength = find_dubins_path_length(
+        startPosition, startHeading, evaderPosition, turnRadius
+    )
+    return dubinsPathLength < (captureRadius + pursuerRange)
+
+
 def plot_dubins_engagement_zone(
     startPosition,
     startHeading,
@@ -192,6 +254,7 @@ def plot_dubins_engagement_zone(
     y = np.linspace(-2, 2, numPoints)
     [X, Y] = np.meshgrid(x, y)
     Z = np.zeros(X.shape)
+    collisionRegion = np.zeros(X.shape)
 
     for i in range(X.shape[0]):
         for j in range(X.shape[1]):
@@ -206,6 +269,17 @@ def plot_dubins_engagement_zone(
                 evaderHeading,
                 evaderSpeed,
             )
+            collisionRegion[i, j] = collision_region(
+                startPosition,
+                startHeading,
+                turnRadius,
+                captureRadius,
+                pursuerRange,
+                pursuerSpeed,
+                evaderSpeed,
+                np.array([X[i, j], Y[i, j]]),
+            )
+
             # Z[i, j] = temp_BEZ(
             #     startPosition,
             #     pursuerRange,
@@ -221,20 +295,46 @@ def plot_dubins_engagement_zone(
     ax = plt.gca()
     ax.set_aspect("equal", "box")
     plt.scatter(*startPosition, c="g")
+    theta = np.linspace(0, 2 * np.pi, 100)
+    leftCenter = np.array(
+        [
+            startPosition[0] - turnRadius * np.sin(startHeading),
+            startPosition[1] + turnRadius * np.cos(startHeading),
+        ]
+    )
+    rightCenter = np.array(
+        [
+            startPosition[0] + turnRadius * np.sin(startHeading),
+            startPosition[1] - turnRadius * np.cos(startHeading),
+        ]
+    )
+    leftX = leftCenter[0] + turnRadius * np.cos(theta)
+    leftY = leftCenter[1] + turnRadius * np.sin(theta)
+    rightX = rightCenter[0] + turnRadius * np.cos(theta)
+    rightY = rightCenter[1] + turnRadius * np.sin(theta)
+    plt.plot(leftX, leftY, "b")
+    plt.plot(rightX, rightY, "b")
+    plt.contour(X, Y, collisionRegion, levels=[0])
     return ax
 
 
 def main():
     startPosition = np.array([0, 0])
-    startHeading = np.pi / 2
-    turnRadius = 1.0
+    startHeading = 0
+    turnRadius = 0.5
     captureRadius = 0.1
     pursuerRange = 1
     pursuerSpeed = 2
     evaderSpeed = 1
     agentHeading = 0
 
-    # find_dubins_path_length(startPosition, startHeading, goalPosition, radius)
+    # agentPosition = np.array([1.5, -0.5])
+    #
+    # length = find_dubins_path_length(
+    #     startPosition, startHeading, agentPosition, turnRadius
+    # )
+    # print("Length: ", length)
+
     ax = plot_dubins_engagement_zone(
         startPosition,
         startHeading,
@@ -256,30 +356,6 @@ def main():
         ax,
     )
     plt.show()
-
-    # p1 = np.array([3, 4])
-    # c = np.array([1, -2])
-    #
-    # r = 0.5
-    #
-    # pt = find_counter_clockwise_tangent_point(p1, c, r)
-    # pcw = find_clockwise_tangent_point(p1, c, r)
-    #
-    # #     # Draw the circle
-    # theta = np.linspace(0, 2 * np.pi, 100)
-    # x = c[0] + r * np.cos(theta)
-    # y = c[1] + r * np.sin(theta)
-    # plt.plot(x, y, "b")
-    # plt.scatter(*c, c="r")
-    # plt.scatter(*p1, c="g")
-    # plt.scatter(*pt, c="y")
-    # plt.scatter(*pcw, c="y")
-    # plt.plot([pt[0], p1[0]], [pt[1], p1[1]], "r")
-    # plt.plot([pcw[0], p1[0]], [pcw[1], p1[1]], "r")
-    #
-    # ax = plt.gca()
-    # ax.set_aspect("equal", "box")
-    # plt.show()
 
 
 if __name__ == "__main__":
