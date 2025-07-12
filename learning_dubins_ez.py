9a03b0d310db41b58e6b87b47c6a79a1d0bc9d91
ggs24 <grastagg33@gmail.com>
2025-07-12 17:59:48 -0600

    working on smoothing ez

--- learning_dubins_ez.py
+++ learning_dubins_ez.py
@@ -2184,7 +2184,7 @@ def plot_filtered_box_rmse_and_abs_errors(
 if __name__ == "__main__":
     results_dir = "results/knownShapeAndSpeed"
     # results_dir = "results/knownSpeed"
-    plot_median_rmse_and_abs_errors(results_dir, max_steps=10, epsilon=0.1)
+    # plot_median_rmse_and_abs_errors(results_dir, max_steps=10, epsilon=0.1)
     # plot_box_rmse_and_abs_errors(results_dir, max_steps=10, epsilon=0.1)
     # plot_filtered_box_rmse_and_abs_errors(results_dir, max_steps=10, epsilon=0.05)
     # main()
